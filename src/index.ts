26a66417ef43056eee88bc128d33dea71affb532
Tobi Burnett <tobiburnett@gmail.com>
2020-01-13 14:56:21 -0500

    Spacing and Declarations

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ async function populateCommand(command: string, required = false){
   commandConfig = await files.readSubConfig(command);
   USAGE[command] = {};
   USAGE[command].config = commandConfig;
+
   // Dont add general help text if command is required for new project generation
   if(!required){
     USAGE.general.menu[1].content.push({
@@ -27,6 +28,7 @@ async function populateCommand(command: string, required = false){
       summary: commandConfig.description,
     });
   }
+
   USAGE[command].menu = CONFIG.USAGE_TEMPLATE(undefined, command, undefined);
   if (commandConfig.arguments !== undefined && commandConfig.arguments !== []) {
     USAGE[command].menu.splice(1, 0, {
@@ -43,12 +45,15 @@ async function populateCommand(command: string, required = false){
 }
 
 async function populateUsage(commands: string[], requiredCommands: string[], mainConfig: any) {
+  let commandConfig: any = {};
+
   USAGE.general = {};
   USAGE.general.menu = CONFIG.USAGE_TEMPLATE();
   USAGE.general.menu.splice(1, 0, {
     header: 'Features',
     content: [],
   });
+
   // Add project config to USAGE
   USAGE.general.menu[1].content.push({
     name: `${chalk.magenta('project')}`,
@@ -63,8 +68,9 @@ async function populateUsage(commands: string[], requiredCommands: string[], mai
   }
 
   commands.push('project');
-  let commandConfig: any = {};
+   
   USAGE.project = {};
+
   commandConfig = mainConfig;
   commandConfig.name = 'project';
   commandConfig.arguments = [
@@ -80,6 +86,7 @@ async function populateUsage(commands: string[], requiredCommands: string[], mai
       'isPrivate': true
     }
   ];
+
   USAGE.project.config = commandConfig;
 }
 
@@ -90,25 +97,29 @@ const run = async () => {
     const mainConfig = await files.readMainConfig();
     const commands: string[] = mainConfig.import.optional;
     const requiredCommands: string[] = mainConfig.import.required;
+    // Check for user arguments
+    const userArgs = process.argv.slice(2);                         
+    const operation: any = {};
+    let project;
+
+    // Declare project here
+
     // Populate command usage information
     await populateUsage(commands, requiredCommands, mainConfig);
 
-    // Check for user arguments
-    const userArgs = process.argv.slice(2);
-
     util.heading();
     if (util.hasCommand(userArgs, commands)) {
-      const operation: any = {};
       operation.command = util.parseCommand(userArgs, commands);
       operation.options = util.parseOptions(userArgs, commands);
 
-      const project = util.checkProjectValidity(operation);
+      project = util.checkProjectValidity(operation);
       if (project.isValid) {
         await MODULE_NEW.run(operation, USAGE);
       } else {
         throw Error(`'${process.cwd()}' is not a valid Vue project.`);
       }
-    } else { // Show help text
+    } else { 
+      // Show Help Text
       console.log(util.displayHelp(USAGE.general.menu));
     }
     process.exit();
