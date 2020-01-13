7ed33aced75c5aa4f5754ac274d86221180f8bad
Tobi Burnett <tobiburnett@gmail.com>
2020-01-13 14:50:17 -0500

    Modified spacing and moved declaration of variable

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -15,45 +15,56 @@ function heading(): void {
     )
   );
 }
+
 function sectionBreak(): void {
   console.log(chalk.gray("********************************"));
 }
+
 function lineBreak(): void {
   console.log('\n');
 }
+
 function nextSteps(featureName: string): void {
   console.log(chalk.magenta('\nNext Steps:'));
   console.log(` - cd ${featureName}\n - npm install\n - npm run-script serve`);
 }
+
 function hasCommand(args: string[], commands: string[]): boolean {
   // console.log(`hasCommand: ${commands}`);
   const found = commands.some((r) => args.includes(r));
   return found;
 }
+
 function hasOptions(args: string[], options: string[]): boolean {
   // console.log(`hasOptions: ${options}`);
   const found = options.some((r) => args.includes(r));
   return found;
 }
+
 function hasHelpOption(args: string[]): boolean {
   // console.log(`hasHelpOptions: ${helpOptions}`);
   const found = helpOptions.some((r) => args.includes(r));
   return found;
 }
+
 function hasInvalidOption(args: string[], options: string[]): boolean {
   // console.log(`hasInvalidOption: ${args}`);
   const found = args.some((r) => !options.includes(r) && !helpOptions.includes(r));
   return found;
 }
+
 function parseCommand(args: string[], commands: string[]): string {
   return args.filter(x => commands.includes(x))[0];
 }
+
 function parseOptions(args: string[], commands: string[]): string[] {
   return args.filter(x => !commands.includes(x));
 }
+
 function displayHelp(sections: Section[]): string {
   return commandLineUsage(sections);
 }
+
 function getKebabCase(str: string) {
 
   const regex = /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g;
@@ -65,9 +76,11 @@ function getKebabCase(str: string) {
 
   return result;
 }
+
 function getPascalCase(str: string) {
   return (str.replace(/\w\S*/g, m => `${m.charAt(0).toLocaleUpperCase()}${m.substr(1).toLocaleLowerCase()}`))
 }
+
 function hasKebab(str = '') {
   let result = false;
   if (str.match(/kebab/gi) !== null) {
@@ -99,7 +112,6 @@ function isRootDirectory(location: string | null = null): boolean {
 
   return isRoot;
 }
-
 
 function getProjectRoot() {
   const configFileName = '.rdvue';
@@ -135,12 +147,13 @@ function checkProjectValidity(operation: any) {
     isValid: false,
     projectRoot: null as any,
   };
+  let projectRoot: string | null;
 
   if (operation.command === 'project') {
     results.isValid = true;
   } else {
 
-    const projectRoot = getProjectRoot();
+    projectRoot = getProjectRoot();
     if (projectRoot !== null) {
       results.isValid = true;
       results.projectRoot = projectRoot;
