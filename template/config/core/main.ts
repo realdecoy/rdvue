4b5dc27e9b5acd379df85b1c16e0070fdf8bdd70
Tobi Burnett <tobiburnett@gmail.com>
2020-01-20 14:24:37 -0500

    Update core to reference "pages" instead of "page"

--- template/config/core/main.ts
+++ template/config/core/main.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from '@/page/app/app.vue';
+import App from '@/pages/app/app.vue';
 import router from '@/config/router';
 import store from '@/store';
 import '@/config/register-service-worker';
