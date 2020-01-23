f9133dacb49268f5af07f6f8959d0f022da5efcc
Tobi Burnett <tobiburnett@gmail.com>
2020-01-23 10:34:09 -0500

    Added interfaces used in modules/new/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { Config } from '../types/usage';
+
 /*
  Please note:
  Linked interfaces have the parent interface followed by there name
@@ -42,4 +44,37 @@ export interface FilesContent{
 export interface Command{
     command: string;
     options: string[];
+}
+
+// The interfaces below are being used in ./modules/new/index.ts
+
+/**
+ * Used to specify both source directories and destination directories
+ * in file operations. eg: copyAndUpdateFiles()
+ */
+export interface Directories {
+    sourceDir: string;
+    installDir: string;
+}
+
+/**
+ * Used to store both the pascal and kebab cases of the feature name
+ * specified by the user arguments
+ */
+export interface featureNameObject {
+    [key: string]: string;
+}
+
+/**
+ * The input necessary to obtain the correct source and install directories
+ * to be utilized for the specific feature requested by user.
+ */
+export interface GetDirectoryInput {
+    featureNameStore: featureNameObject;
+    currentConfig: Config;
+    kebabNameKey: string;
+    isConfig: boolean;
+    isStore: boolean;
+    projectRoot: string | null;
+    userCommand: string;
 }
