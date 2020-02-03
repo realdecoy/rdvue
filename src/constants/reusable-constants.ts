61655e320908f1c443b5dc0f036482b1bca5934d
Tyrone <tyronetaylorbiz@gmail.com>
2020-02-03 11:07:27 -0500

    fiex the generate shortcut action

--- src/constants/reusable-constants.ts
+++ src/constants/reusable-constants.ts
@@ -7,7 +7,7 @@ import gitUserName from 'git-user-name';
 export const DEFAULT_PROJECT_NAME = 'my-vue-app';
 export const REGEX_PROJECT_NAME = /^\s+$/;
 export const GENERATE_ACTION = 'generate';
-export const GENERATE_ACTION_SHORTCUT = 'generate';
+export const GENERATE_ACTION_SHORTCUT = 'g';
 export const TEMPLATE_PROJECT_URL = `https://${gitUserName()}@bitbucket.org/realdecoyteam/rd-vue-cli.git`;
 export const OPTIONS_ALL: string[] = [GENERATE_ACTION];
 export const spinnerIcons = ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷'];
