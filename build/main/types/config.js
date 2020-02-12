751928681b3f58edc29cd610a48f2d38e8cbdbd2
Andre Vidal <avidal@realdecoy.com>
2020-02-11 21:36:01 -0500

    remove build folder from repo

--- build/main/types/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*
-    Types for the config.ts file in the module/new folder
-    for the answer and question object
-
-    not used at the moment
-*/
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL3R5cGVzL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7Ozs7O0VBS0UifQ==
