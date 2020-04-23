7e1d9e0b0bf3139cce6b6580ac1c20e728f4f35d
vidalCreates <amc12vidal@hotmail.com>
2020-04-23 18:46:49 -0500

    update index page copy and line spacing, remove unnecessary class

--- template/config/app/app.vue
+++ template/config/app/app.vue
@@ -1,7 +1,7 @@
 <style lang="scss" src="./app.scss"></style>
 <script lang="ts" src="./app.ts"></script>
 <template>
-  <div id="app" class="bg-gray-400">
+  <div id="app">
     <h1>Vue Starter Project</h1>
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
@@ -21,7 +21,7 @@
         <h3>Summary</h3>
         <span>
           The rdvue cli tool was created as a way to automate many of the processes that go into building SPAs (Single Page Applications) and setting consistencies between projects, shortenening the learning curve for developers per project.
-          The VueJS project will be scaffolded using best-practice structures, dependencies, meta tooling for consistent coding standards, automatic unit test generation (basic ones) and the foundation for E2E testing into a single project.
+          This VueJS project is scaffolded using predetermined best-practice structures, dependencies, meta tooling for consistent coding standards, automatic unit test generation and the foundation for E2E testing.
         </span>
       </div>
       <div class="features">
@@ -62,17 +62,15 @@
               typechecking for javascript (<a href="https://www.typescriptlang.org/">read more</a>)
             </span>
           </li>
+          <li>
+            axios
+            <span class="sub-text">-</span>
+            <span class="desc">
+              promise based http client for browser and node (<a href="https://github.com/axios/axios/">read more</a>)
+            </span>
+          </li>
         </ul>
       </div>
     </div>
-    <main>
-      <transition :duration="500" name="fade" mode="out-in">
-        <!--  
-          Home page and other routes will be dynamically loaded here 
-          by the router.
-        -->
-        <router-view />
-      </transition>
-    </main>
   </div>
 </template>
