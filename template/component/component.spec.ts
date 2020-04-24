15c6b2bf0d2587995d64617afb677840e1ef18ac
OHarris23 <oharris@realdecoy.com>
2020-04-24 13:53:09 -0500

    Updated test file in component folder of the template

--- template/component/component.spec.ts
+++ template/component/component.spec.ts
@@ -2,6 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 import __COMPONENT__ from './__COMPONENT__KEBAB__.vue';
 
 describe('__COMPONENT__.vue', () => {
-  it('mounts', () => {
+  it('mounts', async () => {
+    // Mount component
+    const wrapper = shallowMount(__COMPONENT__);
   });
 });
