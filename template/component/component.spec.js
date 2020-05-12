332c4ae9ff94f1ab7386703e273a8935f0332343
OHarris23 <oharris@realdecoy.com>
2020-05-12 17:31:13 -0500

    Updated unit test configuration for generated components

--- template/component/component.spec.js
+++ template/component/component.spec.js
@@ -2,8 +2,10 @@ import { shallowMount } from '@vue/test-utils';
 import __COMPONENT__ from './__COMPONENT__KEBAB__.vue';
 
 describe('__COMPONENT__.vue', () => {
-  it('mounts', async () => {
-    // Mount component
+  it('mounts component and check if component exists', () => {
+    // Mount component and check if component exists
     const wrapper = shallowMount(__COMPONENT__);
+
+    expect(wrapper.exists()).toEqual(true);
   });
 });
