5c581faedb8826818d8ddbebe6bc8418eb4b5ed7
Andre Vidal <amc12vidal@hotmail.com>
2020-06-10 23:00:52 -0500

    Hotfix/update files included in project config (#54)
    * 0.6.0
    
    * hotfix: update router config syntax, default layout syntax and include theme files in template config manifest

--- template/config/core/router.ts
+++ template/config/core/router.ts
@@ -16,7 +16,7 @@ export default new Router({
       meta: {
         // Specify the layout to use for this page. All pages will use
         // the 'default' layout if not explicity specified here.
-        layout: 'default'
+        layout: 'default',
       },
       // Route level code-splitting is used encapsulate all the code
       // needed for a page into it's own JS file. This greatly speeds-up
