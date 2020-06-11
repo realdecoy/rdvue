b2e7ac22f68398df0d035d19525547f8a9b61e32
Carl Clarke <carlclarke@outlook.com>
2020-06-10 21:06:44 -0500

    Fp 193/tailwind support (#52)
    * 0.6.0
    
    * Moved package.json into Config modules "meta" directory.
    
    * Added vue config with build optimizations.
    - moved cypress.json into cypress dir for cleanliness.
    
    * FP-188: added missing dependencies.
    
    * FP-193: Add Tailwind support.
    
    Co-authored-by: vidalCreates <amc12vidal@hotmail.com>

--- template/config/core/main.ts
+++ template/config/core/main.ts
@@ -3,6 +3,7 @@ import App from '@/pages/app/app.vue';
 import router from '@/config/router';
 import store from '@/store';
 import '@/config/register-service-worker';
+import '@/theme/_all.scss';
 
 Vue.config.productionTip = false;
 
