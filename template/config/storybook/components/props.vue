ba6044fa8079ac68e2f11af780f72f95a4ec09eb
Taslan Graham <gtaslan1@gmail.com>
2020-08-20 22:12:58 -0500

    updated tslint cnfiguration to allow no-any and no-console

--- template/config/storybook/components/props.vue
+++ template/config/storybook/components/props.vue
@@ -30,12 +30,10 @@ export default class Props extends Vue {
   }
 
   private getType(propName: string) {
-    // tslint:disable-next-line:ban-types
-    const typeDef = this.items[propName]?.type as
+     const typeDef = this.items[propName]?.type as
       | { name: string }
       | Array<Function>;
-    // tslint:disable-next-line:no-any
-    let typeName = (typeDef as any)?.name as string | undefined;
+     let typeName = (typeDef as any)?.name as string | undefined;
     const values = StoryProp.getValues(this.source, propName);
 
     if (typeDef instanceof Array) {
