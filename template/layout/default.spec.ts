3ae6d6cb8284d6ecd10a22003666a49f157eada4
Kevonia <KTomlinson@realdecoy.com>
2020-09-28 13:06:47 -0500

    add layout command

---
+++ template/layout/default.spec.ts
@@ -0,0 +1,8 @@
+// Import { shallowMount } from '@vue/test-utils';
+// import Default from './default.vue';
+
+describe('___LAYOUT__KEBAB__.vue', () => {
+  it('mounts', () => {
+    return true;
+  });
+});
