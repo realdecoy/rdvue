5665f0cd45e85b7aa9b7deb7647a568b8b444267
Danyel Roper <danyelroper66@gmail.com>
2020-11-13 15:38:18 -0500

    plugin output fixed

--- src/modules/new/index.ts
+++ src/modules/new/index.ts
@@ -378,7 +378,7 @@ async function run(operation: Command, USAGE: CLI): Promise<any> {
              util.sectionBreak();
              // tslint:disable-next-line:no-console
              
-             // If answer is undefined log a different output message to the CLI
+             // [12] If answer is undefined log a different output message to the CLI
              answers[nameKey] === undefined?
                 console.log(chalk.magenta
                     (`The ${userFeature} plugin has been added to your project.`)):
