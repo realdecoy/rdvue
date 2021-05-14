1d3e296179e45330de5a170f1ecf005ec3acb487
Andre Vidal <amc12vidal@hotmail.com>
2021-05-13 21:39:58 -0500

    add new command to help menu

--- src/help.ts
+++ src/help.ts
@@ -15,6 +15,7 @@ export default class MyHelpClass extends Help {
             create-project   -  Scaffold a new rdvue project
             add              -  Add a feature to a project
             plugin           -  Inject a utility to extend project functionality
+            upgrade          -  Specify the rdvue template version for a project
             
         Options:
             --help | -h      -  Show help information`
