c2c2c38af658d60ef68871da08516a9180930d50
Alex Van Leyen <alex.vanleyen@gmail.com>
2021-06-10 17:12:29 +0000

    chore: fixed lint errors

--- test/lib/files.read-and-update-feature-files.test.ts
+++ test/lib/files.read-and-update-feature-files.test.ts
@@ -6,19 +6,18 @@ import { Files } from '../../src/modules';
 import { readAndUpdateFeatureFiles } from '../../src/lib/files';
 
 describe('lib/files.readAndUpdateFeatureFiles', () => {
-
   const matchRegex = '__VALUE__';
   const destDir = '';
   const dummyFile: { [key: string]: string } = {
     'file1.ts': `line 1\n${matchRegex}\nline 3\n`,
     'file2.ts': `${matchRegex}\nline 2\nline 3\n`,
     'file3.ts': `line 1\nline 2\n${matchRegex}\n`,
-  }
+  };
   let readFileSyncStub: sinon.SinonStub;
   let writeFileSyncStub: sinon.SinonStub;
 
   beforeEach(() => {
-    readFileSyncStub = sinon.stub(fs, 'readFileSync').callsFake((path) => {
+    readFileSyncStub = sinon.stub(fs, 'readFileSync').callsFake(path => {
       return dummyFile[path.toString()];
     });
     writeFileSyncStub = sinon.stub(fs, 'writeFileSync');
@@ -27,7 +26,7 @@ describe('lib/files.readAndUpdateFeatureFiles', () => {
   it('reads and updates feature files', async () => {
     const kebabName = 'component-name';
     const templateVariableName = 'componentName';
-    const pascalName = 'withPascalName'
+    const pascalName = 'withPascalName';
 
     const files: Array<Files> = [
       {
@@ -43,6 +42,7 @@ describe('lib/files.readAndUpdateFeatureFiles', () => {
         target: 'file2.ts',
         content: [{
           matchRegex,
+          // eslint-disable-next-line no-template-curly-in-string
           replace: '${withTemplateVariable}',
         }],
       },
@@ -60,15 +60,15 @@ describe('lib/files.readAndUpdateFeatureFiles', () => {
     expect(writeFileSyncStub.calledWith(...[
       `${destDir}${files[0].target}`,
       `line 1\n${kebabName}\nline 3\n`,
-    ])).to.be.true;
+    ])).to.equal(true);
     expect(writeFileSyncStub.calledWith(...[
       `${destDir}${files[1].target}`,
       `${templateVariableName}\nline 2\nline 3\n`,
-    ])).to.be.true;
+    ])).to.equal(true);
     expect(writeFileSyncStub.calledWith(...[
       `${destDir}${files[2].target}`,
       `line 1\nline 2\n${pascalName}\n`,
-    ])).to.be.true;
+    ])).to.equal(true);
   });
 
   it('copies files?', async () => {
@@ -79,7 +79,7 @@ describe('lib/files.readAndUpdateFeatureFiles', () => {
       {
         source: 'file1.ts',
         target: 'file1.ts',
-      }
+      },
     ];
 
     await readAndUpdateFeatureFiles(destDir, files, kebabName, templateVariableName);
